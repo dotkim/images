51f21d71769238301adee5d5ee4f185d359c2535
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-08-09 05:59:53 +0200

    reworked filters, site alignment, image grid and pagination

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-function toggleState() {
-  let nsfw = document.getElementById("nsfwcheck");
-  if (nsfw.checked == false) {
+function filterState() {
+  let button = document.getElementById('filter');
+  let filter = window.localStorage.getItem('filter');
+  if (filter === 'true') {
+    button.setAttribute('class', 'btn btn-danger');
+    button.textContent = 'Remove filter';
+    return 1;
+  }
+  else if (filter === 'false') {
+    button.setAttribute('class', 'btn btn-info');
+    button.textContent = 'Set filter';
     return 0;
   }
   else {
+    window.localStorage.setItem('filter', true);
+    button.setAttribute('class', 'btn btn-danger');
+    button.textContent = 'Remove filter';
     return 1;
+  }
+}
+
+function buttonClick() {
+  let filter = window.localStorage.getItem('filter');
+  if (filter === 'true') {
+    window.localStorage.setItem('filter', false);
+    location.reload();
+  }
+  else {
+    window.localStorage.setItem('filter', true);
+    location.reload();
   }
 }
 
@@ -16,75 +39,77 @@ function paginationElement(num, content) {
   return elem;
 }
 
-function getImages(page) {
-  fetch('http://api.knerli.no/images/?page=' + page)
+function getImages(page, filter) {
+  fetch('http://api.knerli.no/images/?page=' + page + '&filter=' + filter)
     .then((response) => {
       return response.json();
     })
     .then((imageObj) => {
       let canvas = document.getElementById('canvas');
-
       //Loop over the images and create the grid elements
       imageObj.images.forEach((image) => {
         let div = document.createElement('div');
-        div.setAttribute('class', 'grid-item');
-
+        div.setAttribute('class', 'col-xs m-3 d-flex justify-content-center');
+        div.setAttribute('style', 'width: 200px;');
+        
         let link = document.createElement('a');
         link.setAttribute('href', image.url);
-
+        
         let img = document.createElement('img');
         img.setAttribute('src', image.thumbnail);
-
+        
         //Append all the elements to the canvas
         link.append(img);
         div.append(link);
         canvas.append(div);
       });
-
+      
       //Remove the style attrib, this will make the canvas visible
       //Currently not working as intended.
       canvas.removeAttribute('style');
-
-
+      
+      
       let pagination = document.getElementById('pagination');
       let nav = document.createElement('nav');
       let ul = document.createElement('ul');
-
+      
       ul.setAttribute('class', 'pagination justify-content-center');
-
+      
       let firstLink = paginationElement(1, 'First');
       ul.append(firstLink);
-
+      
       if (imageObj.previous) {
         let prevLink = paginationElement(imageObj.previous, 'Previous');
         ul.append(prevLink);
       }
-
+      
       let i;
-
+      
       for (i = imageObj.current_page - 5; i <= imageObj.current_page - 1; i++) {
         if (i <= 0) continue;
         let page = paginationElement(i, String(i));
         ul.append(page);
       }
-
+      
       for (i = imageObj.current_page; i <= imageObj.current_page + 4; i++) {
-        if (i == imageObj.number_of_pages) return;
+        if (i > imageObj.number_of_pages) continue;
         let page = paginationElement(i, String(i));
         if (i == imageObj.current_page) {
           page.id = 'currentPage';
         }
         ul.append(page);
       }
-
+      
       if (imageObj.next) {
         let nextLink = paginationElement(imageObj.next, 'Next');
         ul.append(nextLink);
       }
-
+      
       let lastLink = paginationElement(imageObj.number_of_pages, 'Last');
       ul.append(lastLink);
 
+      console.log(ul);
+      console.log(nav);
       nav.append(ul);
       pagination.append(nav);
     });
@@ -103,10 +128,11 @@ function getUrlVars() {
 
 window.onload = function () {
   let q = getUrlVars();
+  let filter = filterState();
 
   if (!q.page) {
     q.page = 1
   }
 
-  getImages(q.page);
+  getImages(q.page, filter);
 }
